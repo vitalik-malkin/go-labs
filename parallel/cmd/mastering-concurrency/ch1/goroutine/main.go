65539b30a136752913121d09230de7f456820118
Vitalik Mironov <me@vitalik-mironov.space>
2020-07-07 21:44:30 +0300

    ...

--- parallel/cmd/mastering-concurrency/ch1/goroutine/main.go
+++ parallel/cmd/mastering-concurrency/ch1/goroutine/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
+	"net"
+	"os"
 	"runtime"
 	"strconv"
+	"strings"
+	"syscall"
 	"time"
 )
 
@@ -12,7 +18,10 @@ var (
 )
 
 func main() {
-	ch <- donothing()
+	l1, err := selectTCPListener()
+	l2, err := selectTCPListener()
+	fmt.Printf("%v, %v, %v", l1, l2, err)
+	// ...
 	fmt.Println("done")
 }
 
@@ -44,4 +53,44 @@ func ex1() {
 	runtime.Gosched()
 
 	fmt.Println("done")
+}
+
+func selectTCPListener() (net.Listener, error) {
+	const maxAttempts = 1
+	const portNumRangeVolume = float64(45535)
+	const minPortNum = 20000
+	attemptNum := 0
+	for {
+		portNum := int(math.Ceil(rand.Float64()*portNumRangeVolume)) + minPortNum
+		lis, err := net.Listen("tcp4", fmt.Sprintf(":%v", portNum))
+		if err == nil {
+			return lis, nil
+		}
+		if attemptNum++; isErrorAddressAlreadyInUse(err) && attemptNum < maxAttempts {
+			continue
+		}
+		return nil, err
+	}
+}
+
+func isErrorAddressAlreadyInUse(err error) bool {
+	const windowsAddrInUserErrno = 10048
+	opError, ok := err.(*net.OpError)
+	if !ok {
+		return false
+	}
+	syscallError, ok := opError.Err.(*os.SyscallError)
+	if !ok {
+		return false
+	}
+	syscallErrno, ok := syscallError.Err.(syscall.Errno)
+	if !ok {
+		return false
+	}
+	switch strings.ToUpper(runtime.GOOS) {
+	case "WINDOWS":
+		return syscallErrno == windowsAddrInUserErrno
+	default:
+		return syscallErrno == syscall.EADDRINUSE
+	}
 }
