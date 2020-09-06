98abf41715ce9d36d31f5980a639110f7fd26306
Vitalik Mironov <me@vitalik-mironov.space>
2020-09-06 23:25:06 +0300

    ...

--- etcd/concurrency/cmd/mutex/main.go
+++ etcd/concurrency/cmd/mutex/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	crnd "crypto/rand"
+	"flag"
 	"fmt"
 	"log"
-	"math/rand"
+	"math"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
+	"unsafe"
 
 	etcd_cli "github.com/coreos/etcd/clientv3"
 	etcd_conc "github.com/coreos/etcd/clientv3/concurrency"
@@ -35,7 +39,8 @@ type opState struct {
 }
 
 type clientState struct {
-	name string
+	name       string
+	lockTTLSec int
 
 	ctx context.Context
 	op  chan opState
@@ -46,6 +51,25 @@ type clientState struct {
 var cli *etcd_cli.Client
 
 func main() {
+	var (
+		clientNameFlag string
+		lockTTLSecFlag int
+	)
+	flag.StringVar(&clientNameFlag, "client-name", "", "client name")
+	flag.IntVar(&lockTTLSecFlag, "lock-ttl", -1, "lock time to live period")
+	flag.Parse()
+
+	clientName := strings.ToUpper(clientNameFlag)
+	lockTTLSec := lockTTLSecFlag
+
+	if len(clientName) < 1 {
+		flag.Usage()
+		log.Fatal("no client name specified")
+	} else if lockTTLSec < 0 {
+		flag.Usage()
+		log.Fatal("lock time to live period cannot be less than 0")
+	}
+
 	mainCtx, cancel := context.WithCancel(context.Background())
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
@@ -65,31 +89,44 @@ func main() {
 		log.Fatal(err)
 	}
 
-	cl1 := clientState{
-		name: "C-1",
-		ctx:  mainCtx,
-		op:   make(chan opState),
-		stop: make(chan struct{}, 1),
+	cl := clientState{
+		name:       clientName,
+		lockTTLSec: lockTTLSec,
+		ctx:        mainCtx,
+		op:         make(chan opState),
+		stop:       make(chan struct{}, 1),
 	}
-	go lockLoop(cl1)
+	go lockLoop(cl)
 
-	cl2 := clientState{
-		name: "C-2",
-		ctx:  mainCtx,
-		op:   make(chan opState),
-		stop: make(chan struct{}, 1),
-	}
-	go lockLoop(cl2)
-
-	go workLoop(cl1)
-	go workLoop(cl2)
+	go workLoop(cl)
 
 	<-mainCtx.Done()
 	clr.FgBlue.Println("\nexiting...")
-	<-cl1.stop
-	clr.FgBlue.Printf("%s: done\n", cl1.name)
-	<-cl2.stop
-	clr.FgBlue.Printf("%s: done\n", cl2.name)
+	<-cl.stop
+	clr.FgBlue.Printf("%s: done\n", cl.name)
+
+	clr.Info.Println("Bye...")
+}
+
+func nextRnd(min, maxExclusive int32) (val int32) {
+	rangeSize := int64(maxExclusive) - int64(min)
+	if rangeSize < 1 {
+		panic(fmt.Errorf("invalid range [%d; %d)", min, maxExclusive))
+	} else if rangeSize == 1 {
+		return min
+	}
+
+	bufArr := [4]byte{}
+	buf := bufArr[:]
+	_, err := crnd.Read(buf)
+	if err != nil {
+		panic(fmt.Errorf("error while using crypto-rand: %w", err))
+	}
+	genVal := *(*uint32)(unsafe.Pointer(&bufArr))
+	genLen := int(math.Ceil(math.Log10(float64(genVal))))
+	mul := float64(genVal) * math.Pow10(-genLen)
+	addition := int32(float64(rangeSize) * mul)
+	return min + addition
 }
 
 func workLoop(cl clientState) {
@@ -101,14 +138,14 @@ func workLoop(cl clientState) {
 		default:
 			time.Sleep(time.Second)
 
-			timeoutMs := int64(rand.Float32()*3000.0) + 1000
+			timeoutMs := int64(nextRnd(1000, 5000))
 			err := placeLock(cl.ctx, cl, lckName, timeoutMs)
 			if err != nil {
 				clr.Error.Printf("%s (WORK): timeout=%d, %v\n", cl.name, timeoutMs, err)
 				continue
 			}
 
-			timeoutMs = int64(rand.Float32()*7000.0) + 3000
+			timeoutMs = int64(nextRnd(12000, 20000))
 			clr.Yellow.Printf("%s (WORK): duration=%d, doing work...\n", cl.name, timeoutMs)
 			time.Sleep(10 * time.Second)
 
@@ -161,6 +198,7 @@ func lockLoop(cl clientState) error {
 
 	session, err := etcd_conc.NewSession(
 		cli,
+		etcd_conc.WithTTL(cl.lockTTLSec),
 	)
 	if err != nil {
 		clr.Error.Printf("error while creating session for '%s': %v", cl.name, err)
@@ -173,50 +211,56 @@ func lockLoop(cl clientState) error {
 
 	clr.Info.Printf("%s: started\n", cl.name)
 
+	isSessionInvalid := false
 	muses := make(map[string]*etcd_conc.Mutex)
 
 	for {
 		select {
 		case <-cl.ctx.Done():
 			return cl.ctx.Err()
+		case <-session.Done():
+			isSessionInvalid = true
 		case op := <-cl.op:
-			muPfx := fmt.Sprintf("/%s/", op.lock.name)
+			opErr := error(nil)
+			if isSessionInvalid {
+				opErr = fmt.Errorf("invalid session")
+			} else {
+				muPfx := fmt.Sprintf("/%s/", op.lock.name)
+				switch op.code {
+				case lockOpCode:
+					mu, ok := muses[muPfx]
+					if !ok {
+						mu = etcd_conc.NewMutex(session, muPfx)
+						muses[muPfx] = mu
+					}
+					err := mu.Lock(op.ctx)
+					if err == nil {
+						clr.Info.Printf("%s: lock('%s'), SUCCESS\n", cl.name, op.lock.name)
+					} else {
+						clr.Error.Printf("%s: lock('%s'), FAIL: %v\n", cl.name, op.lock.name, err)
+					}
 
-			switch op.code {
-			case lockOpCode:
-				mu, ok := muses[muPfx]
-				if !ok {
-					mu = etcd_conc.NewMutex(session, muPfx)
-					muses[muPfx] = mu
+					op.resp <- err
+				case unlockOpCode:
+					var err error
+					mu, ok := muses[muPfx]
+					if !ok {
+						err = fmt.Errorf("consistency violation: no mutex found by name '%s'", muPfx)
+					}
+					if err == nil {
+						err = mu.Unlock(op.ctx)
+					}
+					if err == nil {
+						clr.Info.Printf("%s: unlock('%s'), SUCCESS\n", cl.name, op.lock.name)
+					} else {
+						clr.Error.Printf("%s: unlock('%s'), FAIL: %v\n", cl.name, op.lock.name, err)
+					}
+				default:
+					opErr = fmt.Errorf("unknown op code")
 				}
-				err := mu.Lock(op.ctx)
-				if err == nil {
-					clr.Info.Printf("%s: lock('%s'), SUCCESS\n", cl.name, op.lock.name)
-				} else {
-					clr.Error.Printf("%s: lock('%s'), FAIL: %v\n", cl.name, op.lock.name, err)
-				}
-
-				op.resp <- err
-			case unlockOpCode:
-				var err error
-				mu, ok := muses[muPfx]
-				if !ok {
-					err = fmt.Errorf("consistency violation: no mutex found by name '%s'", muPfx)
-				}
-				if err == nil {
-					err = mu.Unlock(op.ctx)
-				}
-				if err == nil {
-					clr.Info.Printf("%s: unlock('%s'), SUCCESS\n", cl.name, op.lock.name)
-				} else {
-					clr.Error.Printf("%s: unlock('%s'), FAIL: %v\n", cl.name, op.lock.name, err)
-				}
-				op.resp <- err
-			default:
-				op.resp <- fmt.Errorf("unknown op code")
 			}
+			op.resp <- opErr
 		}
 
 	}
-
 }
