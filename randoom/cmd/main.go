39c5a66356604e5d15ab6644c67d11eb9c25a741
Vitalik Mironov <me@vitalik-mironov.space>
2021-04-11 12:22:01 +0300

    randoom: +

---
+++ randoom/cmd/main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bufio"
+	cr "crypto/rand"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"unsafe"
+)
+
+const (
+	seedFile = "./seed.txt"
+)
+
+type seed []int
+
+type Field struct {
+}
+
+func main() {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Printf("error: %v", err)
+		}
+	}()
+
+	s, err := loadSeed()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fieldSet := s.nextRandom20FieldSet(4, 30)
+	numStat := make([]int, 20)
+	fmt.Print("Sets:\n")
+	for i := 0; i < len(fieldSet); i++ {
+		if i == 0 {
+			fmt.Print("1) ")
+		} else {
+			if i%2 == 0 {
+				fmt.Print("\n")
+				fmt.Printf("%d) ", i/2+1)
+			} else {
+				fmt.Print("; ")
+			}
+		}
+		fmt.Printf("%v", fieldSet[i])
+
+		for u := 0; u < len(fieldSet[i]); u++ {
+			numStat[fieldSet[i][u]-1] = numStat[fieldSet[i][u]-1] + 1
+		}
+	}
+	fmt.Print("\nStat:\n")
+	for i, v := range numStat {
+		fmt.Printf("%d: %d\n", i+1, v)
+	}
+
+	os.Exit(0)
+}
+
+func loadSeed() (seed, error) {
+	file, err := os.Open(seedFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scan := bufio.NewScanner(file)
+	scan.Split(bufio.ScanLines)
+
+	r := []int{}
+	for scan.Scan() {
+		var n int
+		n, err = strconv.Atoi(scan.Text())
+		if err != nil {
+			return nil, err
+		}
+		r = append(r, n)
+	}
+
+	return r, nil
+}
+
+func (s seed) Len() int {
+	return len(s)
+}
+
+func (s seed) nextRandom() int {
+	var buf [8]byte
+	n, err := cr.Read(buf[:])
+	if err != nil {
+		log.Fatalf("error while populating buffer with random bytes: %v", err)
+	} else if n != len(buf) {
+		log.Fatal("invalid result of random function")
+	}
+	var num1, num2 int32
+
+	bufPtr := unsafe.Pointer(&buf)
+
+	num1 = (*(*int32)(bufPtr)) & int32(0x7fffffff)
+	num2 = (*(*int32)(unsafe.Pointer(uintptr(bufPtr) + unsafe.Sizeof(num1)))) & 0x7fffffff
+	var _ = num2
+
+	return s[int(num1)%len(s)]
+}
+
+func (s seed) nextRandom20() int {
+	return s.nextRandom()%20 + 1
+}
+
+func (s seed) nextRandom20FieldSet(fieldSize, setSize int) [][]int {
+
+	numOverallRepeatLim := (fieldSize * setSize) / 20
+	numOverFieldRepeatLim := 1
+	numAttemptLim := (fieldSize * setSize)
+	fieldAttemptLim := numOverallRepeatLim
+
+	fieldSetAttempt := 0
+nextFieldSet:
+	fieldSetAttempt++
+	fmt.Printf("===============\nNew field set (attempt %d)...\n", fieldSetAttempt)
+
+	numStat := make([]int, 20)
+	newField := make([]int, fieldSize)
+	fieldSet := make([][]int, setSize)
+	for j := 0; j < len(fieldSet); j++ {
+
+		fieldAttempt := 0
+	nextField:
+		fieldAttempt++
+		if fieldAttempt > fieldAttemptLim {
+			goto nextFieldSet
+		}
+
+		for i := 0; i < len(newField); i++ {
+			numAttempt := 0
+		nextNum:
+			numAttempt++
+			if numAttempt > numAttemptLim {
+				goto nextFieldSet
+			}
+			num := s.nextRandom20()
+			if numStat[num-1] >= numOverallRepeatLim {
+				goto nextNum
+			}
+			for y := 0; y < len(newField); y++ {
+				sub := newField[y] - num
+				if sub < 2 && sub > -2 {
+					goto nextNum
+				}
+			}
+			newField[i] = num
+		}
+
+		for y := 0; y < j; y++ {
+			existingField := fieldSet[y]
+			numMatchCount := 0
+			for k := 0; k < len(newField); k++ {
+				for l := 0; l < len(existingField); l++ {
+					if newField[k] == existingField[l] {
+						numMatchCount++
+					}
+				}
+			}
+			if numMatchCount > numOverFieldRepeatLim {
+				goto nextField
+			}
+		}
+
+		for _, num := range newField {
+			numStat[num-1] = numStat[num-1] + 1
+		}
+		sort.Ints(newField)
+		fieldSet[j] = newField
+		newField = make([]int, fieldSize)
+	}
+	fmt.Print("done!\n\n")
+
+	return fieldSet
+}
---
+++ randoom/go.mod
@@ -0,0 +1,3 @@
+module github.com/vitalik-malkin/go-labs/randoom
+
+go 1.16
